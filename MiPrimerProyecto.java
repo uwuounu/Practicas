d765790f305310654bd65b08be7689ccbc33e3d7
uwuounu <143724520+uwuounu@users.noreply.github.com>
2023-09-07 09:53:49 -0500

    Add files via upload

---
+++ MiPrimerProyecto.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miprimerproyecto;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MiPrimerProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo!");
+        System.out.println("Untrust us");
+        System.out.println("");
+              
+    }
+    
+}
